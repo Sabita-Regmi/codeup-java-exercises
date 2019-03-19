aeff337d89b71165bb4c03e041f8cf64c3441c26
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-03-19 12:30:12 -0500

    curriculum exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,42 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavNumber=8;
+        System.out.println(myFavNumber);
+        String myString="i am sabita";
+        System.out.println(myString);
+        float myNumber= 3;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        System.out.println(++x);
+//        System.out.println(x);
+        double calculate=21.0/2.0;
+        System.out.println(calculate);
+        boolean isTrue=false;
+        System.out.println(isTrue);
+        char character='s';
+        System.out.println(character);
+        long abc=3;
+        System.out.println(abc);
+//        int x = 4;
+//        x +=5;
+//        System.out.println(x);
+
+
+//        int x = 3;
+//        int y = 4;
+//        y *=3;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x  /= y;
+        y  -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
