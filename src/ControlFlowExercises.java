0d1e8cb4b41e7c01a5f7333d85f386285899b5f3
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-03-21 15:38:06 -0500

    bonus exercise added

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,21 +20,21 @@ public class ControlFlowExercises {
 
 
 //      #######solution####
-//        int i = 5;
+//        long i = 5;
 //        while (i <= 15) {
-//            System.out.println(+ i);
+//            System.out.print(+ i+" " );
 //            i++;
 //        }
 
 //        Do While
 //
 //        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-        int z = 0;
-        while (z <= 100) {
-            System.out.println(+z);
-            z += 2;
-        }
-//
+//        int z = 0;
+//        while (z <= 100) {
+//            System.out.println(+z);
+//            z += 2;
+//        }
+////
 //
 //        Alter your loop to count backwards by 5's from 100 to -10.
         //#####solution########
@@ -55,12 +55,13 @@ public class ControlFlowExercises {
 //        256
 //        65536
 //######solution########
-//        long x = 2;
-//        do {
-//            System.out.println(x);
-//            x = x * x;
-//        }
-//        while (x < 100000);
+        System.out.println();
+        long x = 2;
+        do {
+            System.out.println(x);
+            x = x * x;
+        }
+        while (x < 100000);
 //fizzbuzz
 
 //    //Fizzbuzz
@@ -80,24 +81,24 @@ public class ControlFlowExercises {
 //            }
 //                System.out.println(k);
 
-        for (int y = 1; y <= 100; y++) {
-            if (y % 3 == 0 && y % 5 == 0) {
-                System.out.println("FizzBuzz");
-
-            } else if (y % 3 == 0) {
-                System.out.println("Fizz");
-
-
-            } else if (y % 5 == 0) {
-                System.out.println("Buzz");
-
-
-            } else {
-                System.out.println(y);
-
-
-            }
-        }
+//        for (int y = 1; y <= 100; y++) {
+//            if (y % 3 == 0 && y % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//
+//            } else if (y % 3 == 0) {
+//                System.out.println("Fizz");
+//
+//
+//            } else if (y % 5 == 0) {
+//                System.out.println("Buzz");
+//
+//
+//            } else {
+//                System.out.println(y);
+//
+//
+//            }
+//        }
 
 
 //        Display a table of powers.
@@ -123,24 +124,24 @@ public class ControlFlowExercises {
 //        5      | 25      | 125
 
         //Display a table of powers
-//        String chooseNumber;
-//        do {
+        String chooseNumber;
+        do {
+
+            System.out.print("What number would you like to go up to?");
+            int userInput = scanner.nextInt();
+            System.out.println("Here is your table!");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= userInput; i++) {
+                int userInputSquared = (int) Math.pow(i, 2);
+                int userInputCubed = (int) Math.pow(i, 3);
+                System.out.format("%-6d | %-7d |  %-8d%n", i, userInputSquared, userInputCubed);
 //
-//            System.out.print("What number would you like to go up to?");
-//            int userInput = scanner.nextInt();
-//            System.out.println("Here is your table!");
-//            System.out.println("number | squared | cubed");
-//            System.out.println("------ | ------- | -----");
-//            for (int i = 1; i <= userInput; i++) {
-//                int userInputSquared = (int) Math.pow(i, 2);
-//                int userInputCubed = (int) Math.pow(i, 3);
-//                System.out.format("%-6d | %-7d |  %-8d%n", i, userInputSquared, userInputCubed);
-////
-//
-//            }
-//            System.out.println("Do you want to print another number?");
-//            chooseNumber = scanner.next();
-//        } while (chooseNumber.equalsIgnoreCase("y") || chooseNumber.equalsIgnoreCase("yes"));
+
+            }
+            System.out.println("Do you want to print another number?");
+            chooseNumber = scanner.next();
+        } while (chooseNumber.equalsIgnoreCase("y") || chooseNumber.equalsIgnoreCase("yes"));
 
 
 //
@@ -208,23 +209,42 @@ public class ControlFlowExercises {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //Bonuses
 
 
 //        create three variables, start, end, and sum. Use a loop to find the summation of all the numbers between start and end (inclusive)
-        int end ;
-        int start ;
-        int sum=0;
-        System.out.println("enter the start number");
-        start=scanner.nextInt();
-        end=scanner.nextInt();
-        System.out.println("enter the end number");
-        for (int i = start; i <= end; i++) {
-            sum = sum + i;
-
-        }
-
-        System.out.println("Sum of the number is: "+sum);
+//        int end ;
+//        int start ;
+//        int sum=0;
+//        System.out.println("enter the start number");
+//        start=scanner.nextInt();
+//        end=scanner.nextInt();
+//        System.out.println("enter the end number");
+//        for (int i = start; i <= end; i++) {
+//            sum = sum + i;
+//
+//        }
+//
+//        System.out.println("Sum of the number is: "+sum);
 
 
 
@@ -233,17 +253,71 @@ public class ControlFlowExercises {
 
 //        2) write a loop which will output all the prime numbers (evenly divisible by only 1 and themselves) between 10 and 10000
         //#####solution########
-        for(int counter=0;counter<=10000;counter++){
-            boolean notPrime=false;
-            for(int i =2;i<=counter;i++){
-                if(counter%i==0 && i!=counter){
-                    notPrime=true;
-                }
+//        for(int counter=0;counter<=10000;counter++){
+//            boolean notPrime=false;
+//            for(int i =2;i<=counter;i++){
+//                if(counter%i==0 && i!=counter){
+//                    notPrime=true;
+//                }
+//            }
+//            if(notPrime==false){
+//                System.out.println(counter);
+//            }
+//        }
+
+        //curriculum bonus
+        boolean confirmation = true;
+        do {
+//
+            System.out.print("Please enter your grade (w/o percentage) here: ");
+            int userNumberGrade = scanner.nextInt();
+            System.out.println();
+            if ((userNumberGrade >= 99) && (userNumberGrade <= 100)) {
+                System.out.println("Congratulations, you scored an A+!!!");
+            } else if ((userNumberGrade >= 94) && (userNumberGrade <= 98)) {
+                System.out.println("Great job, you got an A!!");
+            } else if ((userNumberGrade >= 90) && (userNumberGrade <= 93)) {
+                System.out.println("Nice work, that's an A-!");
+            } else if ((userNumberGrade >= 88) && (userNumberGrade <= 89)) {
+                System.out.println("Nice, that's a B+.");
+            } else if ((userNumberGrade >= 84) && (userNumberGrade <= 87)) {
+                System.out.println("Not bad, that's a B.");
+            } else if ((userNumberGrade >= 80) && (userNumberGrade <= 83)) {
+                System.out.println("Not too bad, that's a B-.");
+            } else if ((userNumberGrade >= 78) && (userNumberGrade <= 79)) {
+                System.out.println("Well, you at least got a C+.");
+            } else if ((userNumberGrade >= 74) && (userNumberGrade <= 77)) {
+                System.out.println("Hey, it could be worse...you got a C.");
+            } else if ((userNumberGrade >= 70) && (userNumberGrade <= 73)) {
+                System.out.println("You barely passed with a C-.");
+            } else if ((userNumberGrade >= 68) && (userNumberGrade <= 69)) {
+                System.out.println("Yikes, you got a D+!");
+            } else if ((userNumberGrade >= 64) && (userNumberGrade <= 67)) {
+                System.out.println("So sorry...you got a D.");
+            } else if ((userNumberGrade >= 60) && (userNumberGrade <= 63)) {
+                System.out.println("Ouch! You got a D-.");
+            } else if ((userNumberGrade >= 0) && (userNumberGrade <= 59)) {
+                System.out.println("Woah...dude. You failed with an F.");
+            } else {
+                System.out.println("Incorrect entry. Please try again.");
             }
-            if(notPrime==false){
-                System.out.println(counter);
+            System.out.println();
+            System.out.print("Would you like to continue? [y/n]");
+            System.out.println();
+            String userResponse = scanner.next();
+            System.out.println("\n");
+            if (userResponse.equalsIgnoreCase("n")) {
+                confirmation = false;
             }
-        }
+
+        } while (confirmation);
+//        3) output a table representing the binary representations of the powers of 2
+//        decimal       |       binary
+//        1     |     00000001
+//        2     |     00000010
+//        4     |     00000100
+
+
 
 
 
