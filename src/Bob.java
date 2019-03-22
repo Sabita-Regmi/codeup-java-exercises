8c234a063e53f70091bc0bfabc64599bf8fa30a0
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-03-22 10:00:16 -0500

    some fixes

--- src/Bob.java
+++ src/Bob.java
@@ -7,21 +7,24 @@ public class Bob {
         Scanner scanner = new Scanner(System.in);
         String bobResponse=" ";
         String questions;
-        System.out.println("Start Conversation: ");
-        questions=scanner.nextLine();
-        if(questions.endsWith("?")){
-            bobResponse="Sure";
-        }
-else if(questions.endsWith("!")){
-    bobResponse="'Whoa, chill out!";
-        }
-else if(questions.isEmpty()){
-    bobResponse="Fine. Be that way!";
+        String answer=" ";
+        do {
+            System.out.println("Start Conversation: ");
+            questions = scanner.nextLine();
+            if (questions.endsWith("?")) {
+                bobResponse = "Sure";
+            } else if (questions.endsWith("!")) {
+                bobResponse = "'Whoa, chill out!";
+            } else if (questions.isEmpty()) {
+                bobResponse = "Fine. Be that way!";
 
-        }else{
-    bobResponse="Whatever";
-        }
-        System.out.println(bobResponse);
+            } else {
+                bobResponse = "Whatever";
+            }
+            System.out.println(bobResponse);
+            System.out.println("Do you want to continue?");
+            answer=scanner.nextLine();
+        }while(answer.equalsIgnoreCase("y")||answer.equalsIgnoreCase("yes"));
     }
 
 }
